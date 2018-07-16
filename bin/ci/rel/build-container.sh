0a3b695df93954b11a257d7e22e1ca2f1118be23
PavelVesnin <pavel.vesnin.v@gmail.com>
2018-07-16 20:16:02 +0300

    [minor] added Dockerfile

--- bin/ci/rel/build-container.sh
+++ bin/ci/rel/build-container.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "[I] Building a Docker container '${PROJECT_NAME}' (version '${PROJECT_VERSION}') from path '${PROJECT_DIR}'.."
 docker build --tag "${PROJECT_NAME}:${PROJECT_VERSION}" \
-             --file "${PROJECT_DIR}/../../Dockerfile" \
+             --file "${PROJECT_DIR}/Dockerfile" \
              --build-arg APP_VERSION=$PROJECT_VERSION \
              --build-arg APP_NAME=$PROJECT_NAME \
              "$PROJECT_DIR/../.."
