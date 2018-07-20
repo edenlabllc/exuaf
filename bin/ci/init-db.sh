0dd4afe6d9dd754651c8d2715b4f451c7d8e841a
PavelVesnin <pavel.vesnin.v@gmail.com>
2018-07-20 16:42:54 +0300

    [patch] take Travis build number

--- bin/ci/init-db.sh
+++ bin/ci/init-db.sh
@@ -2,10 +2,11 @@
 
 POSTGRES_VERSION=9.6
 
+echo "travis TRAVIS_BUILD_NUMBER ${TRAVIS_BUILD_NUMBER}"
 echo "travis TRAVIS_JOB_NUMBER ${TRAVIS_JOB_NUMBER}"
-
-echo "listen_addresses = '*'" >> /etc/postgresql/${POSTGRES_VERSION}/main/postgresql.conf
-echo "host all all 0.0.0.0/0 trust" >> /etc/postgresql/${POSTGRES_VERSION}/main/pg_hba.conf
-
-service postgresql stop
-service postgresql start ${POSTGRES_VERSION}
+#
+#echo "listen_addresses = '*'" >> /etc/postgresql/${POSTGRES_VERSION}/main/postgresql.conf
+#echo "host all all 0.0.0.0/0 trust" >> /etc/postgresql/${POSTGRES_VERSION}/main/pg_hba.conf
+#
+#service postgresql stop
+#service postgresql start ${POSTGRES_VERSION}
